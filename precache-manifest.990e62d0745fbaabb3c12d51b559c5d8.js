81306311c4f656c88f1f774b0f55bacf010989cf
duvancorrea96 <duvancorrea96@gmail.com>
2020-04-23 14:43:35 -0500

    Updates

---
+++ precache-manifest.990e62d0745fbaabb3c12d51b559c5d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3364237b6b0183f31b1fb19e2313f1",
+    "url": "/toolsapp/index.html"
+  },
+  {
+    "revision": "d7cf0b60b1131e72bfac",
+    "url": "/toolsapp/static/css/main.604d5c85.chunk.css"
+  },
+  {
+    "revision": "9dd44b627a8d9f25f8c6",
+    "url": "/toolsapp/static/js/2.3e4dff90.chunk.js"
+  },
+  {
+    "revision": "ef1c1dc921eb892faf80c713a1f15928",
+    "url": "/toolsapp/static/js/2.3e4dff90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7cf0b60b1131e72bfac",
+    "url": "/toolsapp/static/js/main.63947bcd.chunk.js"
+  },
+  {
+    "revision": "47f1c54472ed344b8991",
+    "url": "/toolsapp/static/js/runtime-main.3f7313dc.js"
+  }
+]);
